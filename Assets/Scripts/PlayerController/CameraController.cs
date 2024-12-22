6871d7251efe1ea7cd73fef02ef583450655dad8
Radeon590 <59920979+Radeon590@users.noreply.github.com>
2024-12-23 00:55:04 +0300

    Camera borders

--- Assets/Scripts/PlayerController/CameraController.cs
+++ Assets/Scripts/PlayerController/CameraController.cs
@@ -10,27 +10,62 @@ public class CameraController : MonoBehaviour
     public float maxZoom = 20f; 
     private Vector3 lastMousePosition; // Последняя позиция мыши
 
+    [SerializeField] private Transform minBorder;
+    [SerializeField] private Transform maxBorder;
+
     void Update()
     {
-        // Движение камеры с помощью клавиш
+        #region Движение камеры с помощью клавиш
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        // borders
+        if ((horizontal < 0 && transform.position.x <= minBorder.position.x) || (horizontal > 0 && transform.position.x >= maxBorder.position.x))
+        {
+            horizontal = 0;
+        }
+
+        if (vertical < 0 && transform.position.y <= minBorder.position.y || (vertical > 0 && transform.position.y >= maxBorder.position.y))
+        {
+            vertical = 0;
+        }
+        
+        // movement
         Vector3 movement = new Vector3(horizontal, vertical, 0);
         transform.position += movement * moveSpeed * Time.deltaTime;
+        
+        #endregion
 
-        // Перемещение камеры при зажатии колёсика
+        #region Перемещение камеры при зажатии колёсика
+        
         if (Input.GetMouseButton(2))
         {
             Vector3 delta = Input.mousePosition - lastMousePosition; 
             Vector3 move = new Vector3(-delta.x, -delta.y, 0) * Time.deltaTime; 
+            
+            // borders
+            if ((move.x < 0 && transform.position.x <= minBorder.position.x) || (move.x > 0 && transform.position.x >= maxBorder.position.x))
+            {
+                move = new Vector3(0, move.y, 0);
+            }
+            
+            if ((move.y < 0 && transform.position.y <= minBorder.position.y) || (move.y > 0 && transform.position.y >= maxBorder.position.y))
+            {
+                move = new Vector3(move.x, 0, 0);
+            }
+            
+            // movement
             transform.position += move * moveSpeed * Camera.main.orthographicSize / 10f; 
         }
         lastMousePosition = Input.mousePosition;
+        
+        #endregion
 
-        // Управление зумом с колесика мыши
+        #region Управление зумом с колесика мыши
+        
         float scroll = Input.GetAxis("Mouse ScrollWheel");
         Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - scroll * scrollSpeed, minZoom, maxZoom);
 
+        #endregion
     }
 }
